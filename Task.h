e5a16e33e3eed0bc66d26962c0713ed3face83d2
Abolfazl Ranjbar <110520700+ImAR12@users.noreply.github.com>
2024-01-27 02:49:51 +0330

    Implemented MyBinaryTree and Task classes

---
+++ Task.h
@@ -0,0 +1,29 @@
+#ifndef TASK_H
+#define TASK_H
+
+#include <string>
+#include <vector>
+
+class Task
+{
+public:
+    Task(const std::string& title, const std::string& description, const std::string& deadline, bool isCompleted = false);
+
+    void addSubTask(Task& newTask);
+    bool removeSubTask(Task& target);
+
+    bool operator==(const Task& other) const;
+    bool operator<(const Task& other) const;
+    bool operator>(const Task& other) const;
+
+
+
+private:
+    std::string m_title;
+    std::string m_description;
+    std::string m_deadline;
+    bool m_isCompleted;
+    std::vector<Task> m_subTasks;
+};
+
+#endif // TASK_H
